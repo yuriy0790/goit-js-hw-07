fcdc5b29ae8dfe8f90a792accfc9d6fa9076a68d
Yurii Oborskiy <yuriioborskiy@gmail.com>
2022-11-10 22:14:00 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+
+const galleryImgMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join('');
+listEl.insertAdjacentHTML('beforeend', galleryImgMarkup);
+
+let lightbox = new SimpleLightbox('ul.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
